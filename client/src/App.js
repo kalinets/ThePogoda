ef9446a2b4b7d7c15e523194f301a0d110f4ee28
Uladzimir Kalinets <ukalinets@gmail.com>
2019-10-16 23:47:16 +0300

    update useEffect logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,12 @@
 import React, { Fragment, useState, useEffect } from 'react'
 import { api } from './constants'
-import Skycons from 'react-skycons'
 import './styles.css'
 // https://webdesign.tutsplus.com/tutorials/building-a-weather-app-with-the-darksky-api--cms-28678
 
 export default function App() {
-  const [isGeoAvailable, setIsGeoAvailable] = useState(false)
-  const [[latitude, longitude], setCoords] = useState([,])
+  const [[latitude, longitude], setCoords] = useState([])
   const [weatherForecast, setWeatherForecast] = useState(null)
   const [loading, setLoading] = useState(false)
-
-  const isGeoAllowed = () => {
-    "geolocation" in navigator ? setIsGeoAvailable(true) : setIsGeoAvailable(false)
-  }
-
-  const getLocation = () => {
-    navigator.geolocation.getCurrentPosition(res => {
-      setCoords([res.coords.latitude, res.coords.longitude])
-    })
-  }
-
-  const getWeatherForecast = async () => {
-    try {
-      setLoading(true)
-      const res = await fetch(api.getForecast(latitude, longitude))
-      const json = await res.json()
-      setWeatherForecast(json)
-      console.log('weather forecast', json)
-      setLoading(false)
-    } catch (error) {
-      setLoading(false)
-      throw new Error(error)
-    }
-  }
-
-  const formatDate = dt => {
-    const date = new Date(dt * 1000)
-    return `${date.getDate()}.${date.getMonth() + 1} ${date.getHours()}:${date.getMinutes()}`
-  }
 
   const getDay = dt => {
     const date = new Date(dt * 1000)
@@ -70,123 +39,134 @@ export default function App() {
     )
   }
 
-  useEffect(isGeoAllowed, [])
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(res => {
+      setCoords([res.coords.latitude, res.coords.longitude])
+    })
+  }, [])
+
+  useEffect(() => {
+    let ignore = false
+    async function getData() {
+      setLoading(true)
+      const res = await fetch(api.getForecast(latitude, longitude))
+      const json = await res.json()
+      if (!ignore) setWeatherForecast(json)
+      setLoading(false)
+    }
+    getData()
+    return () => { ignore = true }
+  }, [latitude, longitude])
 
   return (
     <Fragment>
-      <button disabled={!isGeoAvailable} onClick={getLocation}>get your location</button>
-      {latitude && (
-        <div>
-          <p>You are here: latitude is {latitude} and longitude is {longitude}</p>
-          <button disabled={!latitude} onClick={getWeatherForecast}>get weather forecast</button>
-          {loading && <p>Loading...</p>}
-          {weatherForecast && (
-            <Fragment>
-              <h2>Minsk</h2>
-              <p>{weatherForecast.currently.summary}</p>
-              <p>{Math.round(weatherForecast.currently.temperature)}&deg;</p>
-              {getIcon(weatherForecast.currently.icon)}
-              <div className="today-summary">
-                <div>{getDay(weatherForecast.currently.time)} Today</div>
-                <div>
-                  {Math.round(weatherForecast.daily.data[0].temperatureHigh)}
-                  &nbsp;
-                  {Math.round(weatherForecast.daily.data[0].temperatureLow)}
-                </div>
+      {loading && <p>Loading...</p>}
+      {weatherForecast && weatherForecast.latitude && (
+        <Fragment>
+          <h2>{weatherForecast.timezone}</h2>
+          <p>{weatherForecast.currently.summary}</p>
+          <p>{Math.round(weatherForecast.currently.temperature)}&deg;</p>
+          {getIcon(weatherForecast.currently.icon)}
+          <div className="today-summary">
+            <div>{getDay(weatherForecast.currently.time)} Today</div>
+            <div>
+              {Math.round(weatherForecast.daily.data[0].temperatureHigh)}
+              &nbsp;
+              {Math.round(weatherForecast.daily.data[0].temperatureLow)}
+            </div>
+          </div>
+          <hr />
+          <div className="forecast-hours">
+            {weatherForecast.hourly.data.map(hour => (
+              <div key={hour.time} className="forecast-hour">
+                <p>{getHour(hour.time)}:{getMinutes(hour.time)}</p>
+                {getIcon(hour.icon)}
+                <p>{Math.round(hour.temperature)}&deg;</p>
               </div>
-              <hr />
-              <div className="forecast-hours">
-                {weatherForecast.hourly.data.map(hour => (
-                  <div key={hour.time} className="forecast-hour">
-                    <p>{getHour(hour.time)}:{getMinutes(hour.time)}</p>
-                    {getIcon(hour.icon)}
-                    <p>{Math.round(hour.temperature)}&deg;</p>
-                  </div>
-                ))}
+            ))}
+          </div>
+          <hr />
+          <div className="forecast-days">
+            {weatherForecast.daily.data.map(day => (
+              <div key={day.time} className="forecast-day">
+                <span>{getDay(day.time)}</span>
+                {getIcon(day.icon)}
+                <span>{Math.round(day.temperatureHigh)}&emsp;</span>
+                <span>{Math.round(day.temperatureLow)}</span>
               </div>
-              <hr />
-              <div className="forecast-days">
-                {weatherForecast.daily.data.map(day => (
-                  <div key={day.time} className="forecast-day">
-                    <span>{getDay(day.time)}</span>
-                    {getIcon(day.icon)}
-                    <span>{Math.round(day.temperatureHigh)}&emsp;</span>
-                    <span>{Math.round(day.temperatureLow)}</span>
-                  </div>
-                ))}
-              </div>
-              <hr />
-              <p>Today: {weatherForecast.hourly.summary} {weatherForecast.daily.summary}</p>
-              <hr />
-              <div className="sunrise-sunset">
-                <div className="sunrise">
-                  <p>Sunrise</p>
-                  <p>{getHour(weatherForecast.daily.data[0].sunriseTime)}:{getMinutes(weatherForecast.daily.data[0].sunriseTime)}</p>
-                </div>
-                <div className="sunset">
-                  <p>Sunset</p>
-                  <p>{getHour(weatherForecast.daily.data[0].sunsetTime)}:{getMinutes(weatherForecast.daily.data[0].sunsetTime)}</p>
-                </div>
-              </div>
-              <hr />
-              <div className="wet-data">
-                <div className="chance-of-rain">
-                  <p>Chance of rain</p>
-                  <p>{weatherForecast.currently.precipProbability * 100}%</p>
-                </div>
-                <div className="humidity">
-                  <p>Humidity</p>
-                  <p>{weatherForecast.currently.humidity * 100}%</p>
-                </div>
-              </div>
-              <hr />
-              <div className="wind-feels-like">
-                <div className="wind">
-                  <p>Wind</p>
-                  <p>{weatherForecast.currently.windBearing}&deg; {weatherForecast.currently.windSpeed} m/s</p>
-                </div>
-                <div className="feels-like">
-                  <p>Feels like</p>
-                  <p>{Math.round(weatherForecast.currently.apparentTemperature)}&deg;</p>
-                </div>
-              </div>
-              <hr />
-              <div className="water">
-                <div className="precip">
-                  <p>Precipitation</p>
-                  <p>{weatherForecast.currently.precipIntensity * 100} cm</p>
-                </div>
-                <div className="pressure">
-                  <p>Pressure</p>
-                  <p>{weatherForecast.currently.pressure} hPa</p>
-                </div>
-              </div>
-              <hr />
-              <div className="misc">
-                <div className="visibility">
-                  <p>Visibility</p>
-                  <p>{weatherForecast.currently.visibility} km</p>
-                </div>
-                <div className="uv-index">
-                  <p>UV index</p>
-                  <p>{weatherForecast.currently.uvIndex}</p>
-                </div>
-              </div>
-              <hr />
-              <div className="extra">
-                <div className="clouds">
-                  <p>Cloudiness</p>
-                  <p>{Math.round(weatherForecast.currently.cloudCover * 100)}%</p>
-                </div>
-                <div className="ozone">
-                  <p>Ozone</p>
-                  <p>{weatherForecast.currently.ozone} DU</p>
-                </div>
-              </div>
-            </Fragment>
-          )}
-        </div>
+            ))}
+          </div>
+          <hr />
+          <p>Today: {weatherForecast.hourly.summary} {weatherForecast.daily.summary}</p>
+          <hr />
+          <div className="info-block sunrise-sunset">
+            <div className="sunrise">
+              <p>Sunrise</p>
+              <p>{getHour(weatherForecast.daily.data[0].sunriseTime)}:{getMinutes(weatherForecast.daily.data[0].sunriseTime)}</p>
+            </div>
+            <div className="sunset">
+              <p>Sunset</p>
+              <p>{getHour(weatherForecast.daily.data[0].sunsetTime)}:{getMinutes(weatherForecast.daily.data[0].sunsetTime)}</p>
+            </div>
+          </div>
+          <hr />
+          <div className="info-block wet-data">
+            <div className="chance-of-rain">
+              <p>Chance of rain</p>
+              <p>{weatherForecast.currently.precipProbability * 100}%</p>
+            </div>
+            <div className="humidity">
+              <p>Humidity</p>
+              <p>{weatherForecast.currently.humidity * 100}%</p>
+            </div>
+          </div>
+          <hr />
+          <div className="info-block wind-feels-like">
+            <div className="wind">
+              <p>Wind</p>
+              <p>{weatherForecast.currently.windBearing}&deg; {weatherForecast.currently.windSpeed} m/s</p>
+            </div>
+            <div className="feels-like">
+              <p>Feels like</p>
+              <p>{Math.round(weatherForecast.currently.apparentTemperature)}&deg;</p>
+            </div>
+          </div>
+          <hr />
+          <div className="info-block water">
+            <div className="precip">
+              <p>Precipitation</p>
+              <p>{weatherForecast.currently.precipIntensity * 100} cm</p>
+            </div>
+            <div className="pressure">
+              <p>Pressure</p>
+              <p>{weatherForecast.currently.pressure} hPa</p>
+            </div>
+          </div>
+          <hr />
+          <div className="info-block misc">
+            <div className="visibility">
+              <p>Visibility</p>
+              <p>{weatherForecast.currently.visibility} km</p>
+            </div>
+            <div className="uv-index">
+              <p>UV index</p>
+              <p>{weatherForecast.currently.uvIndex}</p>
+            </div>
+          </div>
+          <hr />
+          <div className="info-block extra">
+            <div className="clouds">
+              <p>Cloudiness</p>
+              <p>{Math.round(weatherForecast.currently.cloudCover * 100)}%</p>
+            </div>
+            <div className="ozone">
+              <p>Ozone</p>
+              <p>{weatherForecast.currently.ozone} DU</p>
+            </div>
+          </div>
+        </Fragment>
       )}
+      <a href="https://darksky.net/poweredby/">Powered by Dark Sky</a>
     </Fragment>
   );
 }
