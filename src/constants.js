b5601f9cd09a345ba74b218fa8f9a625d370fb1d
Uladzimir Kalinets <ukalinets@gmail.com>
2019-12-18 19:39:59 +0300

    test cors

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 const API_KEY = 'a853dd25b19367cdd633c26ac7d1bdcd'
 export const api = {
   getForecast(lat, lon) {
-    return `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${API_KEY}/${lat},${lon}?units=si`
+    return `https://api.darksky.net/forecast/${API_KEY}/${lat},${lon}?units=si`
   },
 }
